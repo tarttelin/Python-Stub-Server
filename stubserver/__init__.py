f6bf6217cf6e48d6b789ecc2cfdb2cde89cc16cc
ctarttelin <ctarttelin@equalexperts.com>
2017-09-29 14:18:16 +0100

    Increment the version to push contributed changes to PYPI

--- stubserver/__init__.py
+++ stubserver/__init__.py
@@ -1,11 +1,8 @@
-"""A stub webserver used to enable blackbox testing of applications that call
-external web urls. For example, an application that consumes data from an
-external REST api. The usage pattern is intended to be very much like using
-a mock framework."""
+"""A stub webserver used to enable blackbox testing of applications that call external web urls. For example, an application that consumes data from an external REST api. The usage pattern is intended to be very much like using a mock framework."""
 from stubserver.webserver import StubServer
 from stubserver.ftpserver import FTPStubServer
 
-VERSION = __version__ = '1.0.1'
+VERSION = __version__ = '1.0.2'
 __author__ = 'Chris Tarttelin and Point 2 inc'
 __email__ = 'chris@pyruby.co.uk'
 __url__ = 'http://www.pyruby.com/pythonstubserver'
